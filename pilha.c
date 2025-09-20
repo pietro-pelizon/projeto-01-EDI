a44beba299c06ddd758d3c6a9da728bff69cde88
2-ou-1 <pietrofernandopelizon@gmail.com>
2025-09-20 17:21:14 -0300

    alterações nos arquivos pilha.c e pilha.h

--- pilha.c
+++ pilha.c
@@ -4,18 +4,115 @@
 
 typedef struct node {
     void *item;
-    node *prox;
+    struct node *prox;
 }node;
 
 typedef struct pilha {
-    pilha *topo;
+    node *topo;
     int tam;
 }pilha;
 
-void inicializa(pilha *p) {
+
+
+pilha criaPilha(pilha *p) {
     p -> topo = NULL;
+    p -> tam = 0;
+
+    return *p;
 }
 
 void push(pilha *p, void *item) {
-    pilha *novo = malloc(sizeof(item));
+    if (p == NULL) return;
+
+    node *novo = (node*) malloc(sizeof(node));
+    if (!novo) {
+        printf("erro ao alocar memoria para o noh da pilha\n");
+        exit(1);
+    }
+
+    novo -> item = item;
+    novo -> prox = p -> topo;
+    p -> topo = novo;
+
+    p -> tam++;
+}
+
+void *pop(pilha *p) {
+    if (p == NULL || p -> topo == NULL) {
+        return NULL;
+    }
+
+    node *destroi = p -> topo;
+
+    void *itemDesempilhado = destroi -> item;
+    p -> topo = destroi -> prox;
+    free(destroi);
+
+    p -> tam--;
+
+    return itemDesempilhado;
+}
+
+void *topo(pilha const *p) {
+    if (p == NULL || p -> topo == NULL) {
+        printf("a pilha esta vazia\n");
+        return NULL;
+    }
+
+    return p -> topo -> item;
+}
+
+int estaVazia(pilha const *p) {
+    if (p -> topo == NULL) {
+        return 1;
+    }
+
+    return 0;
+}
+
+void liberaPilha(pilha *p) {
+    if (p == NULL) {
+        return;
+    }
+
+    node *atual = p -> topo;
+
+    while (atual != NULL) {
+        node *proximo = atual -> prox;
+        free(atual);
+        atual = proximo;
+
+        p -> tam--;
+    }
+
+    free(p);
+}
+
+
+int main() {
+    pilha p;
+    criaPilha(&p);
+    int *numero = malloc (sizeof(int));
+    if (!numero) {
+        printf("erro ao alocar memoria para o valor inteiro\n");
+        return 1;
+    }
+
+    *numero = 10;
+
+
+
+    push(&p, numero);
+
+
+    int *numeroNoTopo = topo(&p);
+    int *dadoRemovido = pop(&p);
+
+    printf("dado removido: %d", *dadoRemovido);
+    free(dadoRemovido);
+
+
+
+    free(numero);
+
 }
--- pilha.h
+++ pilha.h
@@ -1,27 +1,28 @@
 #ifndef PILHA_H
 #define PILHA_H
 
-// A definição da struct foi movida para o arquivo .c
+// A definição da struct foi movida para o arquivo.c
 // para encapsular a implementação. O header só precisa
 // saber que o tipo 'pilha' existe.
 typedef struct pilha pilha;
 typedef struct node node;
 
-/// @brief: Inicializa uma pilha, deixando-a pronta para uso.
-/// @param p: Um ponteiro para a struct pilha.
-void inicializa(pilha *p);
 
-/// @brief Insere um elemento no topo da pilha.
-/// @param p Ponteiro para a struct pilha.
-/// @param item Ponteiro para o dado que será inserido na pilha.
+/// @brief: cria uma pilha de alocação dinâmica
+/// @return: retorna um ponteiro para o início da pilha
+pilha *criaPilha();
+
+/// @brief: Insere um elemento no topo da pilha.
+/// @param p: Ponteiro para a struct pilha.
+/// @param item: Ponteiro para o dado que será inserido na pilha.
 void push(pilha *p, void *item);
 
 /// @brief: Remove e retorna o elemento do topo da pilha.
 /// @param p: Ponteiro para a struct pilha.
-/// @return: Retorna o elemento que foi desempilhado. Retorna NULL se a pilha estiver vazia.
+/// @return: Retorna o elemento desempilhado. Retorna NULL se a pilha estiver vazia.
 void *pop(pilha *p);
 
-/// @brief: Retorna o elemento no topo da pilha sem removê-lo.
+/// @brief: Retorna o elemento no topo da pilha sem o remover.
 /// @param p: Ponteiro para a struct pilha.
 /// @return: Retorna o elemento que está no topo. Retorna NULL se a pilha estiver vazia.
 void *topo(pilha *p);
@@ -30,5 +31,18 @@ void *topo(pilha *p);
 /// @param p: Ponteiro para a struct pilha.
 /// @return: Retorna 1 se a pilha estiver vazia, 0 caso contrário.
 int estaVazia(pilha *p);
+
+
+/// @brief: Libera todos os dados dentro da estrutura.
+/// @param p: ponteiro para a pilha a ser liberada.
+void liberaPilha(pilha *p);
+
+/// @brief: Mostra os elementos que estão dentro da devida da pilha.
+/// @param p: Ponteiro para a pilha.
+/// @param imprimeItem: Ponteiro para a função que sabe como imprimir os items.
+/// @param item: parâmetro da função que sabe imprimir os itens.
+void imprimePilha(pilha *p, (*imprimeItem)(void *item));
+
+
 
 #endif //PILHA_H
