5d92fc2c32072c5d6887bbae3d41cfeb690d2936
2-ou-1 <pietrofernandopelizon@gmail.com>
2025-09-28 23:53:59 -0300

    .

---
+++ src/arena.c
@@ -0,0 +1,92 @@
+#include "arena.h"
+#include "fila.h"
+#include "formas.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stArena {
+    double altura, largura;
+    fila *filaArena;
+}arena;
+
+arena *criaArena(double largura, double altura) {
+    arena *a = (arena*) malloc (sizeof(arena));
+    if (a == NULL) {
+        printf("Erro ao criar a arena!\n");
+        return NULL;
+    }
+
+    a -> largura = largura;
+    a-> altura = altura;
+    a -> filaArena = criaFila();
+
+    printf("Arena criada com sucesso!\n");
+
+    return a;
+}
+
+forma *adicionaFormaArena(arena *a, forma *f) {
+    if (f == NULL) {
+        return NULL;
+    }
+
+    if (a == NULL) {
+        printf("Arena inexistente passada para a função 'adicionaFormaArena'\n");
+        return NULL;
+    }
+
+    enqueue(a -> filaArena, f);
+
+    return f;
+
+}
+
+forma *removeFormaArena(arena *a) {
+    if (a == NULL ) return NULL;
+
+    forma *removido = dequeue(a -> filaArena);
+
+    return removido;
+}
+
+bool arenaEstaVazia(arena *a) {
+    if (a == NULL) return true;
+
+    return estaVazia(a -> filaArena);
+}
+
+void destrutorArena(arena **a_ptr) {
+    if (a_ptr == NULL || *a_ptr == NULL) {
+        return;
+    }
+    arena* a = *a_ptr;
+
+    liberaFila(a -> filaArena, destrutorForma);
+
+    free(a);
+    *a_ptr = NULL;
+}
+
+double getLarguraArena(arena *a) {
+    return a -> largura;
+}
+
+double getAlturaArena(arena *a) {
+    return a -> altura;
+}
+
+void setLarguraArena(arena *a, double novaLargura) {
+    a -> largura = novaLargura;
+}
+
+void setAlturaArena(arena *a, double novaAltura) {
+    a -> altura = novaAltura;
+}
+int getArenaNumFormas(arena *a) {
+    return getTamFila(a -> filaArena);
+}
+
+calc
+
+
+
---
+++ src/arena.h
@@ -0,0 +1,76 @@
+#ifndef ARENA_H
+#define ARENA_H
+
+#include "formas.h"
+
+
+ /* ------- TAD ARENA -------
+ * O TAD Arena representa o "mundo" ou "cenário" principal do programa,
+ * funcionando como um container para todas as formas ativas. Seus
+ * principais conceitos são:
+ *
+ * Limites Definidos: A arena é criada com uma largura e altura
+ * específicas, que definem a área visível do cenário.
+ *
+ * Armazenamento FIFO (Fila): As formas são armazenadas em uma estrutura
+ * de Fila (Primeiro que Entra, Primeiro que Sai). Isso garante que, ao
+ * desenhar, as formas mais antigas sejam renderizadas por baixo das mais
+ * novas, criando um efeito de profundidade correto;
+ *
+ * Gerenciamento de Memória: A arena é responsável pelo ciclo de vida das
+ * formas que armazena. Ao ser destruída, todas as formas contidas nela
+ * também são liberadas da memória.
+ */
+
+
+/// A definição da struct fica no arquivo .c
+typedef struct stArena arena;
+
+/// @brief: Cria uma arena com largura e altura.
+/// @param largura: Define o limite horizontal da arena.
+/// @param altura: Define o limite vertical da arena.
+/// @return: Retorna um ponteiro para a arena criada.
+arena *criaArena(double largura, double altura);
+
+/// @brief: Adiciona uma forma à arena.
+/// @param a: Ponteiro para a arena.
+/// @param f: Ponteiro para a forma que será adicionado à arena.
+/// @return: Retorna um ponteiro para a forma adicionada.
+forma *adicionaFormaArena(arena *a, forma *f);
+
+/// @brief: Remove uma forma da arena.
+/// @param a: Ponteiro para a arena.
+/// @param f: Ponteiro para o objeto que será removido.
+/// @return: Retorna um ponteiro para o objeto removido.
+forma *removeFormaArena(arena *a, forma *f);
+
+/// @brief: Libera a arena e todas as formas dentro dela.
+/// @param a: Ponteiro para a arena.
+void destrutorArena(arena **a);
+
+/// @brief: Pega e retorna a largura atual da arena.
+/// @param a: Ponteiro para a arena.
+/// @return: Largura da arena.
+double getLarguraArena(arena *a);
+
+/// @brief: Pega e retorna a altura atual da arena.
+/// @param a: Ponteiro para a arena.
+/// @return: Altura da arena.
+double getAlturaArena(arena *a);
+
+/// @brief: Define uma nova largura para a arena.
+/// @param a: Ponteiro para a arena.
+void setLarguraArena(arena *a, double novaLargura);
+
+/// @brief: Define uma nova altura para arena.
+/// @param a: Ponteiro para a arena.
+void setAlturaArena(arena *a, double novaAltura);
+
+/// @brief: Conta e retorna a quantidade de formas que estão na arena.
+/// @param a: Ponteiro para a arena.
+/// @return: Quantidade de formas na arena.
+int getArenaNumFormas(arena *a);
+
+calc
+
+#endif //ARENA_H
